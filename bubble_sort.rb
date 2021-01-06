5f025c1b07d0ed42c06710d7482ffb8f76b0b3dd
AK-Devil <abdulkhaliql1f18bscs0159@gmail.com>
2021-01-06 22:21:34 +0500

    Remove linters error

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -44,16 +44,7 @@ def bubble_sort_by(data)
   data
 end
 
-<<<<<<< HEAD
 p "Bubbel_sort = #{bubble_sort(arr1)}"
 print 'Bubbel_sort_by = '
 p bubble_sort_by(arr2) { |left, right| left.length - right.length}
 p "Bubbel_sort_by = #{bubble_sort_by(arr1) { |left, right| left - right }}"
-=======
-p ("bubble = #{bubble_sort(arr1)}")
-p bubble_sort(arr2)
-
-print "bubble_by = "
-p bubble_sort_by(arr2){ |left,right| left.length - right.length}
-p ("bubble_by = #{bubble_sort_by(arr1){ |left,right| left - right}}")
->>>>>>> e7a3bab23d9c8513c00b8458fd215dbe88a2d3c6
