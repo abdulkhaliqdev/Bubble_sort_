f82f7106ed7192fe8cf68673ed0e4db56fba667e
AK-Devil <abdulkhaliql1f18bscs0159@gmail.com>
2021-01-06 20:03:50 +0500

    Add Bubble_sort Function in main ruby file

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,7 +1,36 @@
+arr1 = [7,6,5,4,3,2,1]
+arr2 = ["hi", "hey", "hello"]
+
 def bubble_sort(data)
-  puts "returns sorted array"
+  size = data.length
+  if size <= 1
+    return data
+  end
+
+  index = 0
+  flag = true 
+  while index < size && flag == true do 
+    flag = false
+    j = 0
+    while j < size-1 do
+      if data[j] > data[j+1] 
+        data[j],data[j+1]=data[j+1],data[j]
+        flag=true
+      end
+      j += 1
+    end
+
+    index += 1
+  end
+  data
 end
+
+p bubble_sort(arr1)
 
 def bubble_sort_by(data)
   puts "uses the yield method to return the sorted array"
+end
+
+p bubble_sort_by(arr2) do |left,right|
+  left.length - right.length
 end
