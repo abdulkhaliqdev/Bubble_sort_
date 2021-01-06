feb1cf6095e45b158f30ac3c43abc80ac53edc75
AK-Devil <abdulkhaliql1f18bscs0159@gmail.com>
2021-01-06 21:03:14 +0500

    Remove linters error

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,24 +1,21 @@
-arr1 = [7,6,1,5,4,3,2,1]
-arr2 = ["hi", "hello", "hey" ,"a"]
+arr1 = [7, 6, 1, 5, 4, 3, 2, 1]
+arr2 = ['hi', 'hello', 'hey' ,'a']
 
 def bubble_sort(data)
   size = data.length
-
   return data unless size > 1
-
   index = 0
   flag = true 
-  while index < size && flag == true do 
+  while index < size && flag == true 
     flag = false
     j = 0
-    while j < size-1 do
-      if data[j] > data[j+1] 
-        data[j],data[j+1]=data[j+1],data[j]
-        flag=true
+    while j < size-1
+      if data[j] > data[j + 1] 
+        data[j],data[j + 1] = data[j + 1],data[j]
+        flag = true
       end
       j += 1
     end
-
     index += 1
   end
   data
@@ -27,21 +24,19 @@ end
 def bubble_sort_by(data)
   size = data.length
   return data unless size > 1
-
   index = 0
-  flag = true 
+  flag = true
   while index < size && flag == true do 
     flag = false
     j = 0
     while j < size-1 do
       positives = yield data[j], data[j+1]
-      if positives.positive?()
-        data[j],data[j+1]=data[j+1],data[j]
+      if positives.positive?
+        data[j], data[j + 1] = data[j + 1], data[j]
         flag=true
       end
       j += 1
     end
-
     index += 1
   end
   data
